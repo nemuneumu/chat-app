2cdbfe44a9015cc5b362a655d38ac0cf5d979b5b
nemuneumu <netcat344@gmail.com>
2026-03-25 10:46:34 +0900

    フロント実装の準備

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module ChatApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.generator do |g|
+    config.generators do |g|
       g.stylesheets false
       g.javascript false
       g.helper false
